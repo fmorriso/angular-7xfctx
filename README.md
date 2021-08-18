ff87ae9c5fcc163c7df9321e3f9d6b126284db11
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2021-08-18 11:26:09 -0400

    add additional examples

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # angular-material-icon
-# angular-7xfctx
 
-[OLD Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7xfctx)
+Small example of how to use <mat-icon> in Angular via Angular Material
+
+# references
+
+- [mat-icon](https://material.angular.io/components/icon/api)
+- [Google Icons](https://fonts.google.com/icons)
+- [Material Icons Guide](https://developers.google.com/fonts/docs/material_icons)
+
+# history
+
+Cloned 2021-08-18 using Angular 12.x
--- src/app/icon-svg-example.ts
+++ src/app/icon-svg-example.ts
@@ -1,13 +1,15 @@
-import {Component} from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material/icon';
+import { Component, VERSION as ngVersion } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import { VERSION as matVersion } from '@angular/material/core';
 
-const THUMBUP_ICON = `
+const THUMBUP_ICON =
+  `
   <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px">
     <path d="M0 0h24v24H0z" fill="none"/>
     <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.` +
-      `44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5` +
-      `1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/>
+  `44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5` +
+  `1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/>
   </svg>
 `;
 
@@ -25,8 +27,14 @@ export class IconSvgExample {
     // `iconRegistry.addSvgIcon('thumbs-up', sanitizer.bypassSecurityTrustResourceUrl('icon.svg'));`
     iconRegistry.addSvgIconLiteral('thumbs-up', sanitizer.bypassSecurityTrustHtml(THUMBUP_ICON));
   }
-}
+  get angularVersion(): string {
+    return ngVersion.full;
+  }
 
+  get materialVersion(): string {
+    return matVersion.full;
+  }
+}
 
 /**  Copyright 2021 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
